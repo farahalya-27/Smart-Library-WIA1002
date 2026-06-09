b4c9b42fd84dad2016b0a6138a6be04b0aa04eef
farahalya-27 <24001201@siswa.um.edu.my>
2026-06-09 21:25:42 +0800

    Update BorrowStack.java

--- BorrowStack.java
+++ BorrowStack.java
@@ -4,11 +4,13 @@
  */
 package com.mycompany.smartlibrary;
 import java.util.Stack;
+import java.io.Serializable;
+import java.io.*;
 /**
  *
  * @author 24001
  */
-public class BorrowStack {
+public class BorrowStack implements Serializable {
      private Stack<Book> stack = new Stack<>();
     
     public void push (Book b){
@@ -25,6 +27,24 @@ public class BorrowStack {
         for (int i = stack.size()-1 ; i >= 0; i-- ){
             Book b = stack.get(i);
             System.out.println("[ISBN : " + b.getIsbn() + "] " + b.getTitle() + " by " + b.getAuthor());
+        }
+    }
+        public void saveToFile(){
+        try(ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("history.dat"))){
+            os.writeObject(stack);
+        } catch (IOException e) {
+            System.out.println("Error saving history: " + e.getMessage());
+        }
+    }
+    
+    public void loadFromFile(){
+        File file = new File("history.dat");
+        if(file.exists()){
+            try(ObjectInputStream ios = new ObjectInputStream(new FileInputStream(file))){
+               stack = (Stack<Book>) ios.readObject(); 
+            } catch (Exception e) {
+                System.out.println("Error loading history: " + e.getMessage());
+            }
         }
     }
 }
